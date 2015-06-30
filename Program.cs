86fef447cf0b9fd93af587be6aeb65127108aca6
jaypal23 <jaypal.jadeja@gmail.com>
2015-06-30 14:51:45 +0530

    :tada: Added .gitattributes & .gitignore files

--- Program.cs
+++ Program.cs
@@ -15,8 +15,7 @@ namespace demo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-			   Application.Run(new Form1());
+            Application.Run(new Form1());			   
         }
     }
 }
