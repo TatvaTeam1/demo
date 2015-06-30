c72676ab31f2c1ebcb6097d91dda93421161b249
jaypal23 <jaypal.jadeja@gmail.com>
2015-06-30 14:53:33 +0530

    asdf

--- Program.cs
+++ Program.cs
@@ -15,7 +15,9 @@ namespace demo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());			   
+            Application.Run(new Form1());
+            Application.Run(new Form1());	
+		   
         }
     }
 }
